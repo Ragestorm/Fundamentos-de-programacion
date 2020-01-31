4add556b5ea5e53ffa99cb4ab0cf7c2554896b35
Ragestorm <60274831+Ragestorm@users.noreply.github.com>
2020-01-30 21:12:14 -0500

    Add files via upload

---
+++ Tarea4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tarea4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salario ");
+            double Salario = double.Parse(Console.ReadLine());
+
+            if (Salario < 1961314)
+            {
+                Console.WriteLine("Tarifa1");
+            }
+            else if (1961314 <= Salario && Salario <= 3511212)
+            {
+                Console.WriteLine("Tarifa2");
+            }
+            else
+            {
+                Console.WriteLine("Tarifa3");
+            }
+        }
+    }
+}
